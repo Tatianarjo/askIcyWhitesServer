72fc8dcd9ef9ff1a0eb6d0b0b9bfcf4472655851
Tatiana Johnson <Tatianarjo@gmail.com>
2023-01-16 17:32:38 -0600

    added server side to connect frontend and updated packages

--- server.js
+++ server.js
@@ -4,3 +4,49 @@ import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OpenAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+//initializes configuration
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: "Hey from askIcyWhites",
+    })
+})
+
+//backend allows us to have a payload we'll get data from the body
+app.post('/', async (req, res) => {
+    try{
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+//Below sends it back to the frontend
+//If something goes wrong I can see the error via the console.log
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error })
+    }
+})
+
+//Makes sure server always listens to the request
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
